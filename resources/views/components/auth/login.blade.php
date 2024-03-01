67e791ce0650147f23bd2ef7dc8b0094937d2272
cemmydev <cemmy.dev@gmail.com>
2024-02-29 16:52:50 -0800

    finish almost signUp & logIn style

--- resources/views/components/auth/login.blade.php
+++ resources/views/components/auth/login.blade.php
@@ -1,34 +1,45 @@
-<x-auth-card>
-	<x-slot name="logo">
-		<a href="/" class="w-36 h-auto">
-			<x-layout.auth-logo class="fill-current text-gray-500" />
-		</a>
-	</x-slot>
+<x-app-layout>
+	<x-auth-card>
+		<!-- <x-slot name="logo">
+			<a href="/" class="w-36 h-auto">
+				<x-layout.auth-logo class="fill-current text-gray-500" />
+			</a>
+		</x-slot> -->
 
-	<!-- Session Status -->
-	<x-auth-session-status class="mb-4" :status="session('status')" />
+		<!-- Session Status -->
+		<x-auth-session-status class="mb-4" :status="session('status')" />
 
-	<form method="POST" action="{{ route('login') }}">
-		@csrf
-
-		<!-- Email Address -->
-		<div>
-			<x-form.label for="email" :value="__('Email')" />
-			<x-form.input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-			<x-input-error :messages="$errors->get('email')" class="mt-2" />
+		<div class="mb-4 text-center">
+			<div class="mt-2 md:flex md:items-center md:justify-between">
+				<div class="min-w-0 flex-1">
+					<h2 class="text-2xl font-bold leading-7 text-white sm:truncate sm:text-3xl sm:tracking-tight p-3">Login</h2>
+				</div>
+			</div>
 		</div>
 
-		<!-- Password -->
-		<div class="mt-4">
-			<x-form.label for="password" :value="__('Password')" />
-			<div class="relative" x-data="{ showPassword: false }">
-				<input
-					id="password"
-					class="form__input"
-					:type="showPassword === false ? 'password' : 'text'"
-					name="password"
-					required autocomplete="current-password" />
-				<span class="form__input_show-password">
+		<form method="POST" action="{{ route('login') }}">
+			@csrf
+
+			<!-- Email Address -->
+			<div>
+				<x-form.label for="email" :value="__('Email')" />
+				<x-form.input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+				<x-input-error :messages="$errors->get('email')" class="mt-2" />
+			</div>
+
+			<!-- Password -->
+			<div class="mt-4">
+				<x-form.label for="password" :value="__('Password')" />
+				<div class="relative" x-data="{ showPassword: false }">
+					<input
+						class="w-full font-normal bg-transparent !outline-none placeholder:text-foreground-500 focus-visible:outline-none data-[has-start-content=true]:ps-1.5 data-[has-end-content=true]:pe-1.5 text-small"
+						id="password"
+						class="form__input"
+						:type="showPassword === false ? 'password' : 'text'"
+						name="password"
+						required autocomplete="current-password"
+					/>
+					<span class="form__input_show-password">
 						<template x-if="showPassword === false">
 							<x-micon.visibility_on title="{{ __('show password') }}" class="pointer" size="1.125rem" @click="showPassword = true" />
 						</template>
@@ -36,33 +47,34 @@
 							<x-micon.visibility_off title="{{ __('hide password') }}" class="pointer" size="1.125rem" @click="showPassword = false" />
 						</template>
 					</span>
+				</div>
+				<x-input-error :messages="$errors->get('password')" class="mt-2" />
 			</div>
-			<x-input-error :messages="$errors->get('password')" class="mt-2" />
-		</div>
 
-		<!-- Remember Me -->
-		<div class="block mt-4">
-			<label for="remember_me" class="inline-flex items-center">
-				<input id="remember_me" type="checkbox" class="rounded border-gray-300 text-red-600 shadow-sm focus:ring-red-500" name="remember">
-				<span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-			</label>
-		</div>
+			<!-- Remember Me -->
+			<div class="block mt-4">
+				<label for="remember_me" class="inline-flex items-center">
+					<input id="remember_me" type="checkbox" class="rounded border-gray-300 text-red-600 shadow-sm focus:ring-red-500" name="remember">
+					<span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+				</label>
+			</div>
 
-		<div class="flex items-center justify-end mt-4">
-			@if (Route::has('password.request'))
-				<a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500" href="{{ route('password.request') }}">
-					{{ __('Forgot your password?') }}
-				</a>
-			@endif
+			<div class="flex items-center justify-end mt-4">
+				@if (Route::has('password.request'))
+					<a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500" href="{{ route('password.request') }}">
+						{{ __('Forgot your password?') }}
+					</a>
+				@endif
 
-			<x-form.primary-button class="ml-3">
-				{{ __('Log in') }}
-			</x-form.primary-button>
-		</div>
+				<x-form.primary-button class="ml-3">
+					{{ __('Log in') }}
+				</x-form.primary-button>
+			</div>
 
-		<div class="flex items-center justify-center mt-6 gap-2">
-			<span class="text-gray-400">don't have an account?</span>
-			<a href="{{ route('register') }}" class="underline decoration-2 decoration-amber-300 text-gray-600 hover:text-black">Register</a>
-		</div>
-	</form>
-</x-auth-card>
+			<div class="flex items-center justify-center mt-6 gap-2">
+				<span class="text-gray-400">don't have an account?</span>
+				<a href="{{ route('register') }}" class="underline decoration-2 decoration-amber-300 text-gray-600 hover:text-black">Register</a>
+			</div>
+		</form>
+	</x-auth-card>
+</x-app-layout>
--- resources/views/components/auth/register.blade.php
+++ resources/views/components/auth/register.blade.php
@@ -1,90 +1,105 @@
-<x-auth-card>
-	<x-slot name="logo">
-		<a href="/" class="w-48 h-20">
-			<x-layout.auth-logo class="fill-current text-gray-500" />
-		</a>
-	</x-slot>
-
-	<form method="POST" wire:submit.prevent="registerUser">
-		@csrf
-		<!-- Name -->
-		<div>
-			<x-form.label for="name" :value="__('Name')" />
-			<x-form.input
-				id="name"
-				class="block mt-1 w-full"
-				type="text"
-				wire:model.defer="user.name"
-				required
-				autofocus
-			/>
-			<x-form.input-error for="user.name" class="mt-2" />
-		</div>
-
-		<!-- Email Address -->
-		<div class="mt-4">
-			<x-form.label for="email" :value="__('Email')" />
-			<x-form.input
-				id="email"
-				class="block mt-1 w-full"
-				type="email"
-				wire:model.defer="user.email"
-				required />
-			<x-form.input-error for="user.email" class="mt-2" />
-		</div>
-
-		<!-- Password -->
-		<div class="mt-4">
-			<x-form.label for="password" :value="__('Password')" />
-			<div class="relative" x-data="{ showPassword: false }">
-				<input
-					id="password"
-					class="form__input"
-					:type="showPassword === false ? 'password' : 'text'"
-					wire:model.defer="user.password"
-					required
-					autocomplete="new-password" />
-				<span class="form__input_show-password">
-					<template x-if="showPassword === false">
-						<x-micon.visibility_on title="{{ __('show password') }}" class="pointer" size="1.125rem" @click="showPassword = true" />
-					</template>
-					<template x-if="showPassword === true">
-						<x-micon.visibility_off title="{{ __('hide password') }}" class="pointer" size="1.125rem" @click="showPassword = false" />
-					</template>
-				</span>
-			</div>
-			<x-form.input-error for="user.password" class="mt-2" />
-		</div>
-
-		<!-- Confirm Password -->
-		<div class="mt-4">
-			<x-form.label for="password_confirmation" :value="__('Confirm Password')" />
-			<div class="relative" x-data="{ showPassword: false }">
-				<input
-					id="password_confirmation"
-					class="form__input"
-					:type="showPassword === false ? 'password' : 'text'"
-					wire:model.defer="user.password_confirmation"
-					required />
-				<span class="form__input_show-password">
-					<template x-if="showPassword === false">
-						<x-micon.visibility_on title="{{ __('show password') }}" class="pointer" size="1.125rem" @click="showPassword = true" />
-					</template>
-					<template x-if="showPassword === true">
-						<x-micon.visibility_off title="{{ __('hide password') }}" class="pointer" size="1.125rem" @click="showPassword = false" />
-					</template>
-				</span>
-			</div>
-			<x-form.input-error for="user.password_confirmation" class="mt-2" />
-		</div>
-
-		<div class="flex items-center justify-end mt-4">
-			<a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500" href="{{ route('login') }}">
-				{{ __('Already registered?') }}
+<x-app-layout>
+	<x-auth-card>
+		<!-- <x-slot name="logo">
+			<a href="/" class="w-48 h-20">
+				<x-layout.auth-logo class="fill-current text-gray-500" />
 			</a>
-			<x-form.submit-button
-				class="ml-4"
-				:text="__('Register')" />
-		</div>
-	</form>
-</x-auth-card>
+		</x-slot>
+ -->
+
+ 		<div class="mb-4 text-center">
+			<div class="mt-2 md:flex md:items-center md:justify-between">
+				<div class="min-w-0 flex-1">
+					<h2 class="text-2xl font-bold leading-7 text-white sm:truncate sm:text-3xl sm:tracking-tight p-3">SignUp</h2>
+				</div>
+			</div>
+		</div>		
+
+		<form method="POST" wire:submit.prevent="registerUser">
+			@csrf
+			<!-- Name -->
+			<div>
+				<x-form.label for="name" :value="__('Name')" />
+				<x-form.input
+					id="name"
+					class="block mt-1 w-full"
+					type="text"
+					wire:model.defer="user.name"
+					required
+					autofocus
+				/>
+				<x-form.input-error for="user.name" class="mt-2" />
+			</div>
+
+			<!-- Email Address -->
+			<div class="mt-4">
+				<x-form.label for="email" :value="__('Email')" />
+				<x-form.input
+					id="email"
+					class="block mt-1 w-full"
+					type="email"
+					wire:model.defer="user.email"
+					required />
+				<x-form.input-error for="user.email" class="mt-2" />
+			</div>
+
+			<!-- Password -->
+			<div class="mt-4">
+				<x-form.label for="password" :value="__('Password')" />
+				<div class="relative" x-data="{ showPassword: false }">
+					<input
+						class="w-full font-normal bg-transparent !outline-none placeholder:text-foreground-500 focus-visible:outline-none data-[has-start-content=true]:ps-1.5 data-[has-end-content=true]:pe-1.5 text-small"
+						id="password"
+						class="form__input"
+						:type="showPassword === false ? 'password' : 'text'"
+						wire:model.defer="user.password"
+						required
+						autocomplete="new-password" 
+					/>
+					<span class="form__input_show-password">
+						<template x-if="showPassword === false">
+							<x-micon.visibility_on title="{{ __('show password') }}" class="pointer" size="1.125rem" @click="showPassword = true" />
+						</template>
+						<template x-if="showPassword === true">
+							<x-micon.visibility_off title="{{ __('hide password') }}" class="pointer" size="1.125rem" @click="showPassword = false" />
+						</template>
+					</span>
+				</div>
+				<x-form.input-error for="user.password" class="mt-2" />
+			</div>
+
+			<!-- Confirm Password -->
+			<div class="mt-4">
+				<x-form.label for="password_confirmation" :value="__('Confirm Password')" />
+				<div class="relative" x-data="{ showPassword: false }">
+					<input
+						class="w-full font-normal bg-transparent !outline-none placeholder:text-foreground-500 focus-visible:outline-none data-[has-start-content=true]:ps-1.5 data-[has-end-content=true]:pe-1.5 text-small"
+						id="password_confirmation"
+						class="form__input"
+						:type="showPassword === false ? 'password' : 'text'"
+						wire:model.defer="user.password_confirmation"
+						required
+						class="w-full font-normal bg-transparent !outline-none placeholder:text-foreground-500 focus-visible:outline-none data-[has-start-content=true]:ps-1.5 data-[has-end-content=true]:pe-1.5 text-small"/>
+					<span class="form__input_show-password">
+						<template x-if="showPassword === false">
+							<x-micon.visibility_on title="{{ __('show password') }}" class="pointer" size="1.125rem" @click="showPassword = true" />
+						</template>
+						<template x-if="showPassword === true">
+							<x-micon.visibility_off title="{{ __('hide password') }}" class="pointer" size="1.125rem" @click="showPassword = false" />
+						</template>
+					</span>
+				</div>
+				<x-form.input-error for="user.password_confirmation" class="mt-2" />
+			</div>
+
+			<div class="flex items-center justify-end mt-4">
+				<a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500" href="{{ route('login') }}">
+					{{ __('Already registered?') }}
+				</a>
+				<x-form.submit-button
+					class="ml-4"
+					:text="__('Register')" />
+			</div>
+		</form>
+	</x-auth-card>
+</x-app-layout>
