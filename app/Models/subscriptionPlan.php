6560e9fb90d4f1de5095afa09a0c50d79eadc9c4
cemmydev <cemmy.dev@gmail.com>
2024-03-13 16:48:47 +0100

    implement soft delete on subscription plans

--- app/Models/subscriptionPlan.php
+++ app/Models/subscriptionPlan.php
@@ -6,14 +6,20 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use App\Models\Bet;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class subscriptionPlan extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     protected $fillable = [
         "name",
         "cost",
         "description",
+    ];
+
+    protected $casts = [
+        'deleted_at' => 'datetime',
     ];
 
     public function bets() : BelongsToMany {
