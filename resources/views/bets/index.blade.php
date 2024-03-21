248560630a8d033f72942d7a56736bfd52df9bc0
cemmydev <cemmy.dev@gmail.com>
2024-03-21 20:00:15 +0100

    add getting last page function

--- resources/views/bets/index.blade.php
+++ resources/views/bets/index.blade.php
@@ -187,8 +187,8 @@
                                 stroke-linejoin="round" stroke-width="1.5"></path>
                         </svg>
                     </li>
-                    @if($bet['last_page'] <= 5)
-                        @for($i=1; $i<=$bet['last_page']; $i ++)
+                    @if($bets['last_page'] <= 5)
+                        @for($i=1; $i<=$bets['last_page']; $i ++)
                             <a href="{{'?page='.$i}}">
                             <li role="button" tabindex="0" aria-label="pagination item 1 active" aria-current="true"
                                 data-active="true" data-slot="item"
@@ -197,7 +197,7 @@
                             </a>
                             @endfor
                     @else
-                        @if($bet['current_page'] > 5)
+                        @if($bets['current_page'] > 5)
 
                         <li role="button" tabindex="0" aria-label="pagination item 1" data-slot="item"
                             class="tap-highlight-transparent select-none touch-none first-of-type:rounded-r-none last-of-type:rounded-l-none [&amp;:not(:first-of-type):not(:last-of-type)]:rounded-none data-[pressed=true]:scale-[0.97] transition-transform-background flex flex-wrap truncate box-border items-center justify-center text-default-foreground outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 data-[disabled=true]:text-default-300 data-[disabled=true]:pointer-events-none shadow-sm bg-default-100 [&amp;[data-hover=true]:not([data-active=true])]:bg-default-200 active:bg-default-300 min-w-9 w-9 h-9 text-small rounded-medium">
@@ -282,7 +282,7 @@
                         </li>
                         <li role="button" tabindex="0" aria-label="pagination item 49" data-slot="item"
                             class="tap-highlight-transparent select-none touch-none first-of-type:rounded-r-none last-of-type:rounded-l-none [&amp;:not(:first-of-type):not(:last-of-type)]:rounded-none data-[pressed=true]:scale-[0.97] transition-transform-background flex flex-wrap truncate box-border items-center justify-center text-default-foreground outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 data-[disabled=true]:text-default-300 data-[disabled=true]:pointer-events-none shadow-sm bg-default-100 [&amp;[data-hover=true]:not([data-active=true])]:bg-default-200 active:bg-default-300 min-w-9 w-9 h-9 text-small rounded-medium">
-                            {{$bet['last_page']}}</li>
+                            {{$bets['last_page']}}</li>
                         @endif
                     @endif
                     <li role="button" tabindex="0" aria-label="next page button" data-slot="next"
