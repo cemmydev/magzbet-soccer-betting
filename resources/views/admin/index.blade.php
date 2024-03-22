ff132b9c9693048003ebce27364531b02fc2d693
cemmydev <cemmy.dev@gmail.com>
2024-03-22 12:16:04 +0100

    fixed author of admin panel

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="flex items-center justify-between px-4 py-4 border-b lg:py-6 dark:border-primary-darker">
     <h1 class="text-2xl font-semibold">Dashboard</h1>
-    <a href="https://github.com/Kamona-WD/kwd-dashboard" target="_blank"
+    <a href="https://github.com/oleksandr010419/BetTok-soccer_betting" target="_blank"
         class="px-4 py-2 text-sm text-white rounded-md bg-primary hover:bg-primary-dark focus:outline-none focus:ring focus:ring-primary focus:ring-offset-1 focus:ring-offset-white dark:focus:ring-offset-dark">
         View on github
     </a>
