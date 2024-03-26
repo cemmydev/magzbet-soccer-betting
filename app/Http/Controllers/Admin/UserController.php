684dde8b6ef79c71be3029d843f2e36b4283c912
cemmydev <cemmy.dev@gmail.com>
2024-03-26 04:49:25 +0100

    fixed bet categories

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -15,57 +15,65 @@ class UserController extends Controller
     public $usersData, $perPage, $sortByField, $sortByDirection, $search;
     public $subscriptions;
 
-    public function __construct() {
-        $this->usersData=[];
-        $this->sortByField='id';
-        $this->sortByDirection='desc';
-        $this->perPage=15;
-        $this->search= '';
-        $this->subscriptions=subscriptionPlan::all()->toArray();
+    public function __construct()
+    {
+        $this->usersData = [];
+        $this->sortByField = 'id';
+        $this->sortByDirection = 'desc';
+        $this->perPage = 15;
+        $this->search = '';
+        $this->subscriptions = subscriptionPlan::all()->toArray();
     }
-    public function index(Request $request) {
-        $this->sortByField= $request->sortByField;
-        $this->sortByDirection=$request->sortByDirection;
-        $this->perPage=$request->perPage;
-        $this->usersData=User::with('subscriptionPlans')->get()->sortBy($this->sortByField, $this->sortByDirection);
+    public function index(Request $request)
+    {
+        $this->sortByField = $request->sortByField;
+        $this->sortByDirection = $request->sortByDirection;
+        $this->perPage = $request->perPage;
+        $this->usersData = User::with('subscriptionPlans')->get()->sortBy($this->sortByField, $this->sortByDirection);
         return view("admin.users")->with('index', 'index')->with("usersData", $this->usersData)->with('tabIndex', 'users');
     }
 
-    public function create() {
+    public function create()
+    {
         return view('admin.users.create')->with('subscriptions', $this->subscriptions);
     }
 
-    public function delete(Request $request, $id) {
-        $user=User::find($id);
+    public function delete(Request $request, $id)
+    {
+        $user = User::find($id);
         $user->delete();
         return redirect()->route('admin.users');
     }
 
-    public function edit(Request $request, $id) {
+    public function edit(Request $request, $id)
+    {
         $user = User::find($id);
         return view('admin.users')->with('index', 'edit')->with('user', $user);
     }
 
-    public function update(Request $request, $id) {
+    public function update(Request $request, $id)
+    {
         $request->validate([
             'name' => 'required',
             'email' => 'required|email',
         ]);
         $user = User::find($id);
-        $user->name=$request->name;
-        $user->email=$request->email;
+        $user->name = $request->name;
+        $user->email = $request->email;
         $user->save();
         return redirect()->route('admin.users');
     }
 
-    public function expire(Request $request, $id, $sid) {
+    public function expire(Request $request, $id, $sid)
+    {
         DB::table('subscription_plan_user')->where('id', $sid)->update(['expire_at' => date('Y-m-d')]);
-        Toastr::success('User'.$id."'s subscription is expired successfully", "Success!");
+        Toastr::success('User' . $id . "'s subscription is expired successfully", "Success!");
         return redirect()->route('admin.users.edit', $id);
     }
 
-    public function getActiveUsers() {
+    public function getActiveUsers()
+    {
         $active_users = DB::table('sessions')->whereNotNull('user_id')->count();
-        return response()->json(['active'=> $active_users], 200);
+        return response()->json(['active' => $active_users], 200);
     }
 }
--- resources/views/bets/results.blade.php
+++ resources/views/bets/results.blade.php
@@ -18,16 +18,6 @@
                     <div class="flex flex-col gap-3 lg:flex-row lg:items-center">
                         <div class="flex flex-col justify-center gap-1 lg:basis-1/2">
                             <div class="flex items-center gap-3">
-                                {{-- <div class="flex flex-row items-center gap-1 font-bold text-primary"><svg
-                                        xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                                        aria-hidden="true" role="img" class="iconify iconify--mdi" width="1em"
-                                        height="1em" viewBox="0 0 24 24">
-                                        <path fill="currentColor"
-                                            d="m16.93 17.12l-.8-1.36l1.46-4.37l1.41-.47l1 .75v.14c0 .07.03.13.03.19c0 1.97-.66 3.71-1.97 5.21zM9.75 15l-1.37-4.03L12 8.43l3.62 2.54L14.25 15zM12 20.03c-.88 0-1.71-.14-2.5-.42l-.69-1.51l.66-1.1h5.11l.61 1.1l-.69 1.51c-.79.28-1.62.42-2.5.42m-6.06-2.82c-.53-.62-.99-1.45-1.38-2.46c-.39-1.02-.59-1.94-.59-2.75c0-.06.03-.12.03-.19v-.14l1-.75l1.41.47l1.46 4.37l-.8 1.36zM11 5.29v1.4L7 9.46l-1.34-.42l-.42-1.36C5.68 7 6.33 6.32 7.19 5.66s1.68-1.09 2.46-1.31zm3.35-.94c.78.22 1.6.65 2.46 1.31c.86.66 1.51 1.34 1.95 2.02l-.42 1.36l-1.34.43l-4-2.77V5.29zm-9.42.58C3 6.89 2 9.25 2 12s1 5.11 2.93 7.07S9.25 22 12 22s5.11-1 7.07-2.93S22 14.75 22 12s-1-5.11-2.93-7.07S14.75 2 12 2S6.89 3 4.93 4.93">
-                                        </path>
-                                    </svg>
-                                    {{(4-count($result['subscription_plan'])).'/3'}}
-                                </div> --}}
                                 <div class="text-md flex flex-row items-center gap-1 text-sm text-gray-400"><svg
                                         xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                                         aria-hidden="true" role="img" class="iconify iconify--mdi" width="1em"
@@ -170,9 +160,11 @@
                                 <path d="M6 17l5-5-5-5"></path>
                             </svg>
                         </li>
+                        <a href="{{'?page='.$results['last_page']}}">
                         <li role="button" tabindex="0" aria-label="pagination item 49" data-slot="item"
-                            class="tap-highlight-transparent select-none touch-none first-of-type:rounded-r-none last-of-type:rounded-l-none [&amp;:not(:first-of-type):not(:last-of-type)]:rounded-none data-[pressed=true]:scale-[0.97] transition-transform-background flex flex-wrap truncate box-border items-center justify-center text-default-foreground outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 data-[disabled=true]:text-default-300 data-[disabled=true]:pointer-events-none shadow-sm bg-default-100 [&amp;[data-hover=true]:not([data-active=true])]:bg-default-200 active:bg-default-300 min-w-9 w-9 h-9 text-small rounded-medium bg-primary">
+                            class="tap-highlight-transparent select-none touch-none first-of-type:rounded-r-none last-of-type:rounded-l-none [&amp;:not(:first-of-type):not(:last-of-type)]:rounded-none data-[pressed=true]:scale-[0.97] transition-transform-background flex flex-wrap truncate box-border items-center justify-center text-default-foreground outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 data-[disabled=true]:text-default-300 data-[disabled=true]:pointer-events-none shadow-sm bg-default-100 [&amp;[data-hover=true]:not([data-active=true])]:bg-default-200 active:bg-default-300 min-w-9 w-9 h-9 text-small rounded-medium @if($results['current_page']==$results['last_page']) {{bg-primary}} @endif">
                             {{$results['last_page']}}</li>
+                        </a>
                         @endif
                         @endif
                         <li role="button" tabindex="0" aria-label="next page button" data-slot="next"
